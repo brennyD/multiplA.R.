9c70d05dfac3415b213ccee0c3e7e245f8868b8e
Brendan DeMilt <bdemilt98@gmail.com>
2018-04-06 20:45:06 -0400

    Establishment of view hierarchy

---
+++ ARt/MainScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  MainScreenView.swift
+//  ARt
+//
+//  Created by Brendan DeMilt on 4/6/18.
+//  Copyright © 2018 Multiplaugmented Mentalities. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MainScreenView: UIViewController{
+    
+    
+    
+    
+}
+
+
+

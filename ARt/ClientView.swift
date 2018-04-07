d94348c4c374b628155161592a76ede592809c39
Brendan DeMilt <bdemilt98@gmail.com>
2018-04-06 20:13:35 -0400

    Establishment of Client Search state

---
+++ ARt/ClientView.swift
@@ -0,0 +1,18 @@
+
+//
+//  File.swift
+//  ARt
+//
+//  Created by Brendan DeMilt on 4/6/18.
+//  Copyright © 2018 Multiplaugmented Mentalities. All rights reserved.
+//
+
+import Foundation
+
+class ClientView: UIViewController{
+    
+    
+    
+    
+}
+

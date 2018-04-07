17acb02d65a882ea4737e9a6004f5776ae53b20f
Brendan DeMilt <bdemilt98@gmail.com>
2018-04-06 21:08:23 -0400

    Hierarchy

---
+++ ARt/HostView.swift
@@ -0,0 +1,56 @@
+//
+//  HostView.swift
+//  ARt
+//
+//  Created by Brendan DeMilt on 4/6/18.
+//  Copyright © 2018 Multiplaugmented Mentalities. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HostView: UIViewController{
+    
+    
+    @IBOutlet weak var ClientTable: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+      
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+ 
+    
+    
+    
+    
+}
